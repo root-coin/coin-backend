2ab8305a8ac3315685a5e40d15f28ba4008f5cde
Dongjin Lim <42141895+djlim98@users.noreply.github.com>
2021-05-14 06:03:21 +0900

    매수 라우터 생성

--- routes/trade.routes.js
+++ routes/trade.routes.js
@@ -4,8 +4,10 @@ const coin = require('../controllers/coin.controller');
 
 router.get('/coins', coin.getCoinsPrice);
 
-router.get('/chart/:coinId', coin.getCoinPrice);
+router.get('/chart/:coinId', coin.getCoinChart);
 
 router.post('/save-data', coin.saveInfo);
+
+router.post('/transaction/buy/:coinId', auth.verifyToken, coin.buy);
 
 module.exports = router;
