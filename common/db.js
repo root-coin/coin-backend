85c998734d7e11e79673ffc667d494753debf76b
Dongjin Lim <42141895+djlim98@users.noreply.github.com>
2021-05-14 06:06:11 +0900

    오름차순 생성

--- common/db.js
+++ common/db.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const AWS = require('aws-sdk');
 AWS.config = {
   accessKeyId: process.env.aws_access_key_id,
@@ -67,6 +68,7 @@ exports.find = (item, resultCallback) => {
   var params = {
     TableName: 'coin',
     KeyConditionExpression: 'dataType = :d',
+    ScanIndexForward: false,
     FilterExpression: '#id = :id',
     ExpressionAttributeNames: {
       '#id': item.find,
@@ -74,6 +76,32 @@ exports.find = (item, resultCallback) => {
     ExpressionAttributeValues: {
       ':d': { S: item.dataType },
       ':id': { S: item.value },
+    },
+  };
+  console.log(params);
+  ddb.query(params, (err, data) => {
+    if (err) {
+      resultCallback(err, null);
+      return;
+    }
+    resultCallback(null, data.Items);
+  });
+};
+
+exports.readRecent = (item, resultCallback) => {
+  var params = {
+    TableName: 'coin',
+    ScanIndexForward: false,
+    KeyConditionExpression: 'dataType = :d',
+    //KeyConditionExpression: 'dataType = :d and id = :m',
+    FilterExpression: '#id = :id',
+    ExpressionAttributeNames: {
+      '#id': item.find,
+    },
+    ExpressionAttributeValues: {
+      ':d': { S: item.dataType },
+      ':id': { S: item.value },
+      //':m': { S: moment().subtract(1, 'minutes').format('YYYYMMDDHHmm') },
     },
   };
   console.log(params);
