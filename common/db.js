7215b4d92b78d3a3413fbed0430e4b4d875a2fa2
Dongjin Lim <42141895+djlim98@users.noreply.github.com>
2021-05-13 20:10:31 +0900

    data null 처리

--- common/db.js
+++ common/db.js
@@ -1,50 +1,50 @@
 var AWS = require('aws-sdk');
-AWS.config.update({region: 'ap-northeast-2'});
+AWS.config.update({ region: 'ap-northeast-2' });
 var ddb = new AWS.DynamoDB();
 
 exports.create = (item, resultCallback) => {
-    var params = {
-        TableName: 'coin',
-        Item: item
+  var params = {
+    TableName: 'coin',
+    Item: item,
+  };
+
+  console.log(params);
+
+  ddb.putItem(params, function (err, data) {
+    if (err) {
+      resultCallback(err, null);
+    } else {
+      resultCallback(null, data);
     }
-
-    console.log(params);
-
-    ddb.putItem(params, function(err, data){
-        if(err){
-            resultCallback(err, data);
-        } else{
-            resultCallback(null, data);
-        }
-    });
-}
+  });
+};
 
 exports.read = (item, resultCallback) => {
-    var params = {
-        TableName: 'coin',
-        Key: item
-    }
+  var params = {
+    TableName: 'coin',
+    Key: item,
+  };
 
-    ddb.getItem(params, function(err, data){
-        if(err){
-            resultCallback(err, data);
-        } else{
-            resultCallback(null, data);
-        }
-    });
-}
+  ddb.getItem(params, function (err, data) {
+    if (err) {
+      resultCallback(err, null);
+    } else {
+      resultCallback(null, data);
+    }
+  });
+};
 
 exports.delete = (item, resultCallback) => {
-    var params = {
-        TableName: 'coin',
-        Key: item
-    }
+  var params = {
+    TableName: 'coin',
+    Key: item,
+  };
 
-    ddb.deleteItem(params, function(err, data){
-        if(err){
-            resultCallback(err, data);
-        } else{
-            resultCallback(null, data);
-        }
-    });
-}
+  ddb.deleteItem(params, function (err, data) {
+    if (err) {
+      resultCallback(err, null);
+    } else {
+      resultCallback(null, data);
+    }
+  });
+};
