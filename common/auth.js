c86a4b465a0c7ce2ad58c21bedc9aa66e411bfcf
Dongjin Lim <42141895+djlim98@users.noreply.github.com>
2021-05-13 17:02:39 +0900

    토큰 인증 구현

---
+++ common/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const KEY = process.env.TOKEN_KEY;
+
+exports.verifyToken = (req, res, next) => {
+  try {
+    const { loginTK } = req.cookies;
+
+    if (!loginTK) {
+      res.status(401).json({ error: 'Token information is not provided.' });
+      return;
+    }
+
+    const decoded = jwt.verify(loginTK, KEY);
+
+    if (decoded) {
+      res.locals.userId = decoded.userId;
+      res.locals.userPassword = decoded.userPassword;
+      res.locals.userNickname = decoded.userNickname;
+      next();
+    } else {
+      res.status(401).json({ error: 'Token expired.' });
+    }
+  } catch (err) {
+    res.status(401).json({ error: err });
+  }
+};
