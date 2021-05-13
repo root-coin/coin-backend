4b3ac0dd97a4def9337aafb28c637f9ff2e4e263
Dongjin Lim <42141895+djlim98@users.noreply.github.com>
2021-05-13 17:10:30 +0900

    토큰 인증 라우터 체이닝

--- routes/account.routes.js
+++ routes/account.routes.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const user = require('../controllers/user.controller');
+const auth = require('../common/auth');
 
 router.post('/new', user.new);
 
-router.post('/login', user.createToken);
+router.post('/login', auth.verifyToken, user.createToken);
+
+router.get('/logout', user.logout);
 
 module.exports = router;
